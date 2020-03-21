939ea565d89f051b780e490e7e837d3073f76250
Marc Tanis <marcato15gmail.com>
2020-03-21 23:22:27 +0000

    Save changes

--- x/.config/polybar/launch.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-killall polybar
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar top &
-    MONITOR=$m polybar bottom &
-  done
-fi
